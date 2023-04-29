357f75c80131be44a99f5410fea9ccae08d4770b
Coder-Towhid <islamsmtowhidul@gmail.com>
2023-04-29 22:11:32 +0600

    all components loaded

---
+++ src/components/Indecision.jsx
@@ -0,0 +1,57 @@
+import { useState } from "react";
+import Action from "./Action";
+import AddOption from "./AddOption";
+import Header from "./Header";
+import Options from "./Options";
+
+const Indecision = () =>{
+    const [options, setOption] = useState({
+        value:[]
+    })
+
+    const handleDeleteOptions = ()=>{
+        setOption({
+            value:[]
+          });
+    }
+
+   const handlePick = () =>{
+    const randomNum = Math.floor(Math.random()*options.value.length)
+    const option = options[randomNum]
+    alert(option)
+   }
+
+   const handleAddOption =(option)=>{
+    if(!option){
+        return 'Enter valid value to add item'
+    }else if(options.value.indexOf(option)>-1){
+        return 'This option already exists'
+    }
+
+    setOption((prevState) =>{
+        return{
+            value:prevState.value.concat(option)
+        }
+    })
+  
+   }
+   const title = 'indecison '
+   const subtitle = 'put your life in the hands of computer'
+    return(
+        <div>
+        <Header title={title} subtitle={subtitle} />
+        <Action
+        hasOptions={options.length>0}
+            handlePick={handlePick}
+        />
+        <Options
+            options={options}
+            handleDeleteOptions={handleDeleteOptions}
+        />
+        <AddOption
+        handleAddOption={handleAddOption} />
+        
+        </div>
+    )
+}
+export default Indecision
---
+++ src/components/Options.jsx
@@ -0,0 +1,7 @@
+const Options = ()=>{
+    return(
+        console.log("hello")
+    )
+}
+
+export default Options
